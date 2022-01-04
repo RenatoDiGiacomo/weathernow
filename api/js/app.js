9134bf14b1171b857c64371b1b54ef97c4c4b046
RenatoDiGiacomo <renato_di_giacomo@hotmail.com>
2022-01-04 20:26:31 -0300

    Error Tratament made it

--- api/js/app.js
+++ api/js/app.js
@@ -1,34 +1,34 @@
 const vm = new Vue({
     el: "#app",
     data: {
-        // https://api.weatherapi.com/v1/current.json?key=cc6f6d5f143b4962b74161643220401&q=brazil&units=metric
         api_key: "cc6f6d5f143b4962b74161643220401",
         url_base: "http://api.weatherapi.com/v1/current.json",
         query: "",
+        errorStatus: "",
+        errorText:"",
+
+        //items
         weather: {},
         location: {},
         current: {},
         condition: {},
     },
     methods: {
-        takeData(e) {
-            if (e.key === "Enter") {
-                fetch(`${this.url_base}?key=${this.api_key}&q=${this.query}&lang=pt`)
-                    .then((r) => {
+        takeData() {
+            fetch(`${this.url_base}?key=${this.api_key}&q=${this.query}&lang=pt`)
+                .then((r) => {
+                    this.errorStatus = r.status
+                    this.errorText = r.statusText
+                    if (r.ok || r.status != 400) {
                         return r.json();
-                    })
-                    .then(this.results);
-            }
+                    }
+                })
+                .then((response) => {
+                    this.weather = response;
+                    this.current = response.current;
+                    this.location = response.location;
+                    this.condition = response.current.condition;
+                });
         },
-        results(results) {
-            this.weather = results;
-            this.current = results.current;
-            this.location = results.location;
-            this.condition = results.current.condition;
-        },
-        mounted(){
-            this.results()
-        }
-        
     },
 });
